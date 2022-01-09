3ca2650cc49b5f32dd5f5867bf6c1252e43ec179
JulienBe <bertozzijulien@gmail.com>
2022-01-09 18:59:00 +0100

    test if nmae is an image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use regex::Regex;
+use std::env;
 use bevy::prelude::*;
 
 fn main() {
+  let args: Vec<String> = env::args().collect();
   App::build()
     .add_plugins(DefaultPlugins)
     .run();
+}
+
+fn is_valid_image_path(arg: &str) -> bool {
+  return Regex::new(r"\.png$").unwrap().is_match(&arg)
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::is_valid_image_path;
+
+  #[test]
+  fn is_valid_image_path_test() {
+    assert_eq!(is_valid_image_path("pom"), false);
+    assert_eq!(is_valid_image_path(""), false);
+    assert_eq!(is_valid_image_path("/home/julien/image.png"), true);
+  }
 }
