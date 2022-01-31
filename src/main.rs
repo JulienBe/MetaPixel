a9e3f34609246521ed30e794465398d8153e77a3
JulienBe <bertozzijulien@gmail.com>
2022-01-31 21:45:28 +0100

    Read file size

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use regex::{Match, Regex};
-use std::env;
+use regex::{Regex};
+use std::{fs};
+
 use lazy_static::lazy_static;
 
-static aeron_regex: &str = "aeron_file=.*";
+static AERON_REGEX: &str = "aeron_file=.*";
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+  //let args: Vec<String> = env::args().collect();
 }
 
 fn get_aeron_file(arg: &str) -> Option<&str> {
   lazy_static! {
-    static ref mode: Regex = Regex::new(aeron_regex).unwrap();
+    static ref MODE: Regex = Regex::new(AERON_REGEX).unwrap();
   }
   // error: look-around, including look-ahead and look-behind, is not supported
-  if mode.is_match(arg) {
+  if MODE.is_match(arg) {
     let split: Vec<&str> = arg.split('=').collect();
     let last: Option<&&str> = split.last();
     if last.is_some() && !last.unwrap().is_empty() {
@@ -23,14 +24,24 @@ fn get_aeron_file(arg: &str) -> Option<&str> {
   None
 }
 
+fn read_aeron_file(file: &str) -> usize {
+  let data = fs::read(file).expect("Unable to read file");
+  return data.len()
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::get_aeron_file;
+  use crate::{get_aeron_file, read_aeron_file};
 
   #[test]
   fn get_aeron_file_test() {
     assert_eq!(get_aeron_file("aeron_file=pom.dat"), Some("pom.dat"));
     assert_eq!(get_aeron_file("aeron_file="), None);
     assert_eq!(get_aeron_file("pom=pouet"), None);
+  }
+
+  #[test]
+  fn read_aeron_file_test() {
+    assert_eq!(read_aeron_file("resources/test/cnc.dat"), 8392704);
   }
 }
